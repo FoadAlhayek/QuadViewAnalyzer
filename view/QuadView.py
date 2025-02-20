adc9dc3aeac15892049eaa39d8d8d3115b167466
FoadAlhayek <foad_97@hotmail.com>
2025-02-20 11:55:17 +0100

    First template/shell code for the quad viewer

--- view/QuadView.py
+++ view/QuadView.py
@@ -9,8 +9,11 @@ Usage: main.py, QuadViewModel.py
 from PySide6.QtWidgets import QWidget, QToolButton, QLabel, QHBoxLayout, QVBoxLayout, QComboBox, QGridLayout
 from PySide6.QtGui import QIcon, QFontDatabase, QPixmap
 from PySide6.QtCore import Qt
+import pyqtgraph as pg
 import pathlib
 import sys
+from pyqtgraph import GraphicsLayoutWidget
+
 
 # Internal imports
 import assets.widgets as c_widgets
@@ -26,8 +29,8 @@ class QuadView(QWidget):
         ##################
         # Init constants #
         ##################
-        WINDOW_WIDTH = int(900)
-        WINDOW_HEIGHT = int(600)
+        WINDOW_WIDTH = int(1200)
+        WINDOW_HEIGHT = int(800)
         LABEL_FONT_SIZE = "20pt"
         MARGIN_PX = int(30)
 
@@ -40,98 +43,109 @@ class QuadView(QWidget):
         ###############
         # Init the UI #
         ###############
-        self.theme = LightTheme()
+        theme = LightTheme()
         self.setWindowTitle("QuadViewAnalyzer")
         self.setWindowIcon(QIcon(str(pathlib.Path(app_path) / "assets" / "icons" / "gui_logo.ico")))
         self.resize(WINDOW_WIDTH, WINDOW_HEIGHT)
-        self.setStyleSheet(f"background-color: {self.theme.background};")
+        self.setStyleSheet(f"background-color: {theme.background};")
 
         #########################################
         # Define the widgets and their settings #
         #########################################
-        self.button_start_QVA = QToolButton()
+        # Init subplots
+        self.glw = GraphicsLayoutWidget()
 
+        ###################
+        #    TOP LEFT     #
+        #   Video Frames  #
+        ###################
+        self.video_ax = self.glw.addPlot(row=0, col=0, title="Camera POV")
+        self.video_ax.hideAxis("bottom")
+        self.video_ax.hideAxis("left")
+
+        ###################
+        #    TOP RIGHT    #
+        #  Analysis Graph #
+        ###################
+        self.graph_ax = self.glw.addPlot(row=0, col=1, title="Graph analysis")
+
+        ###################
+        #   BOTTOM LEFT   #
+        #   Data Insight  #
+        ###################
+        self.di_ax = self.glw.addPlot(row=1, col=0, title="Data insight")
+        self.di_ax.hideAxis("bottom")
+        self.di_ax.hideAxis("left")
+
+        ###################
+        #  BOTTOM RIGHT   #
+        #  Birdseye View  #
+        ###################
+        self.birds_ax = self.glw.addPlot(row=1, col=1)
+        self.birds_ax.addItem(pg.ScatterPlotItem())
+
+        ###################
+        # SLIDER SETTINGS #
+        ###################
+        self.slider = pg.QtWidgets.QSlider(Qt.Orientation.Horizontal)
+        self.slider.setRange(0, 1000)
+        self.slider.setValue(0)
+
+        ###################
+        #  Other widgets  #
+        ###################
         self.label_mat = QLabel("Select mat")
-
         self.text_box_button_mat = c_widgets.TextBoxWithButton(file_ext_filter="MATLAB file (*.mat)")
 
         ###########################################
         # Connect interactions with the ViewModel #
         ###########################################
-        self.button_start_QVA.clicked.connect(self._view_model.start_qva)
         self.text_box_button_mat.button.clicked.connect(self.update_mat_path)
 
         #####################
         # Style the widgets #
         #####################
-        self.button_start_QVA.setIcon(QIcon(str(pathlib.Path(app_path) / "assets" / "icons" / "power_button.svg")))
-        self.button_start_QVA.setFixedSize(125, 125)
-        self.button_start_QVA.setIconSize(self.button_start_QVA.size())
-        self.button_start_QVA.setStyleSheet('''
-            QToolButton {
-              border-radius: ''' + str(self.button_start_QVA.height() // 2) + ''';
-              border-width: 2px;
-              border-style: solid;
-              border-color: black;
-              background-color: ''' + self.theme.button + ''';
-              text-align: center;
-            }
-            QToolButton:hover {
-              background-color: ''' + self.theme.highlight + ''';
-            }
-        ''')
-
         self.text_box_button_mat.set_size(WINDOW_WIDTH // 3 - int(2 * MARGIN_PX), 50)
         self.text_box_button_mat.setStyleSheet('''
             QLabel {
-              background-color: ''' + self.theme.inactive_button + ''';
-              color: ''' + self.theme.button_text + ''';
+              background-color: ''' + theme.inactive_button + ''';
+              color: ''' + theme.button_text + ''';
             }
         ''')
 
         self.label_mat.setFixedWidth(self.text_box_button_mat.width())
-        self.label_mat.setStyleSheet(f"font-size: {LABEL_FONT_SIZE}; color: {self.theme.label_text}")
+        self.label_mat.setStyleSheet(f"font-size: {LABEL_FONT_SIZE}; color: {theme.label_text}")
 
         #######################################
         # Create the layouts - Order matters! #
         #######################################
-        self.header_layout = QVBoxLayout()
-        self.header_layout.addStretch()
-        self.header_layout.addWidget(self.button_start_QVA)
-        self.header_layout.addStretch()
+        graph_layout = QVBoxLayout()
+        graph_layout.addWidget(self.glw)
 
-        self.config_label_layout = QHBoxLayout()
-        self.config_label_layout.addWidget(self.label_mat)
-
-        self.config_box_layout = QHBoxLayout()
-        self.config_box_layout.addWidget(self.text_box_button_mat)
-
+        test_layout = QVBoxLayout()
+        test_layout.addWidget(self.label_mat)
+        test_layout.addWidget(self.text_box_button_mat)
         ##############################
         # Change alignment & spacing #
         ##############################
-        self.header_layout.setAlignment(self.button_start_QVA, Qt.AlignmentFlag.AlignHCenter)
-
-        self.config_label_layout.setSpacing(MARGIN_PX)
-        self.config_box_layout.setSpacing(MARGIN_PX)
+        test_layout.setSpacing(MARGIN_PX)
 
         ###################
         # Combine layouts #
         ###################
-        self.config_layout = QVBoxLayout()
-        self.config_layout.addLayout(self.config_label_layout)
-        self.config_layout.addLayout(self.config_box_layout)
-        self.config_layout.setContentsMargins(MARGIN_PX, 0, MARGIN_PX, MARGIN_PX)
+        content_layout = QHBoxLayout()
+        content_layout.addLayout(graph_layout)
+        content_layout.addLayout(test_layout)
 
         ######################
         # Create main layout #
         ######################
-        self.main_layout = QVBoxLayout()
-        self.main_layout.addLayout(self.header_layout)
-        self.main_layout.addLayout(self.config_layout)
-        self.main_layout.setContentsMargins(0, 0, 0, 0)
+        main_layout = QVBoxLayout()
+        main_layout.addLayout(content_layout)
+        main_layout.addWidget(self.slider)
 
         # Set the main layout
-        self.setLayout(self.main_layout)
+        self.setLayout(main_layout)
 
     def update_mat_path(self):
         """ Updates the path to the current selected file in the backend """
